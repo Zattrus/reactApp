060886549ad97693b3348e5bc981768c1b9fd8b3
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-10-13 10:38:35 -0300

    Usando componente Card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import "./styles.css";
 
+import { Card } from "../../components/Card";
+
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Lista de presença" />
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
