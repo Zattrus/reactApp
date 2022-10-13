fe19128d13cf568a8fe22ea02401d6f161e38fb0
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-10-13 20:51:58 -0300

    Usando estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 import { Card } from "../../components/Card";
 
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Lista de presença" />
+      <h1>Nome: {studentName}</h1>
+
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Gabriel" time="10:11" />
