8e6111887155fb3ec6002b7fbe50a2c076110489
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-10-13 10:38:13 -0300

    Criando componente

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export function Card() {
+  return (
+    <div className="card">
+      <strong>Gabriel Amancio</strong>
+      <small>10:22:15</small>
+    </div>
+  );
+}
