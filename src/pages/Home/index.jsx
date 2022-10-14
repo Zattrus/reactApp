e5d121903a18dc920e18fd084ff039ef1a8edcc7
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-10-13 21:24:21 -0300

    Aplicando Imutabilidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,21 +5,38 @@ import { Card } from "../../components/Card";
 
 export function Home() {
   const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
 
   return (
     <div className="container">
-      <h1>Nome: {studentName}</h1>
+      <h1>Lista de Presença</h1>
 
       <input
         type="text"
         placeholder="Digite o nome..."
         onChange={(e) => setStudentName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
 
-      <Card name="Gabriel" time="10:11" />
-      <Card name="Leo" time="10:14" />
-      <Card name="Ana" time="13:14" />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card name={student.name} time={student.time} />
+      ))}
     </div>
   );
 }
